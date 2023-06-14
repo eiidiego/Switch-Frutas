446183d7a2e839625ab1f6df012d1f60fcda0917
eiidiego <diegovotu2310@gmail.com>
2023-06-14 14:13:42 -0300

    ip

---
+++ script.js
@@ -0,0 +1,17 @@
+var fruta = "fruta";
+
+fruta = prompt('nome da fruta')
+
+switch (fruta) {
+  case "maçã":
+    alert("Não vendemos esta fruta aqui");
+    break;
+  case "kiwi":
+    alert("Estamos com escassez de kiwis");
+    break;
+  case "melancia":
+    alert("Aqui está, são 3 reais o quilo");
+    break;
+  default:
+    alert("Erro: fruta inválida");
+}
